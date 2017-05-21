737f275d381e3e76b29e7db8d9bec285676dd694
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-05-21 16:17:27 +0200

    renaming of Timeline tests

--- src/test/java/twitter/TimelineTest.groovy
+++ src/test/java/twitter/TimelineTest.groovy
@@ -11,7 +11,7 @@ class TimelineTest extends Specification {
         timeline = new Timeline(clock)
     }
 
-    def 'should have no messages when user has no messages'() {
+    def 'should have no messages when user never posted'() {
         when:
         List<Message> aliceMessages = timeline.view('Alice')
 
@@ -19,7 +19,7 @@ class TimelineTest extends Specification {
         aliceMessages == []
     }
 
-    def 'should store message when user posts a message'() {
+    def 'should have message when user posted a message'() {
         given:
         timeline.post('Alice', 'I love the weather today.')
 
@@ -30,7 +30,20 @@ class TimelineTest extends Specification {
         aliceTimeline.get(0).text == 'I love the weather today.'
     }
 
-    def 'should store two messages when user posts same message twice'() {
+    def 'should have two messages when user posted two different messages'() {
+        given:
+        timeline.post('Bob', 'Damn! We lost!')
+        timeline.post('Bob', 'Good game though.')
+
+        when:
+        List<Message> bobTimeline = timeline.view('Bob')
+
+        then:
+        bobTimeline.get(0).text == 'Damn! We lost!'
+        bobTimeline.get(1).text == 'Good game though.'
+    }
+
+    def 'should have two messages when user posted same message twice'() {
         given:
         timeline.post('Alice', 'I love the weather today.')
         timeline.post('Alice', 'I love the weather today.')
@@ -43,20 +56,7 @@ class TimelineTest extends Specification {
         aliceTimeline.get(1).text == 'I love the weather today.'
     }
 
-    def 'should store two messages when user posts two messages'() {
-        given:
-        timeline.post('Bob', 'Damn! We lost!')
-        timeline.post('Bob', 'Good game though.')
-
-        when:
-        List<Message> bobTimeline = timeline.view('Bob')
-
-        then:
-        bobTimeline.get(0).text == 'Damn! We lost!'
-        bobTimeline.get(1).text == 'Good game though.'
-    }
-
-    def 'should store two messages from two different users'() {
+    def 'should have two messages when two users posted a message each'() {
         given:
         timeline.post('Alice', 'I love the weather today.')
         timeline.post('Bob', 'Damn! We lost!')
@@ -70,7 +70,7 @@ class TimelineTest extends Specification {
         bobTimeline.get(0).text == 'Damn! We lost!'
     }
 
-    def 'should store three messages from two different users'() {
+    def 'should have three messages when two users posted three messages'() {
         given:
         timeline.post('Alice', 'I love the weather today.')
         timeline.post('Bob', 'Damn! We lost!')
@@ -86,9 +86,9 @@ class TimelineTest extends Specification {
         bobTimeline.get(1).text == 'Good game though.'
     }
 
-    def 'should store timestamp with the message when user posts'() {
+    def 'should store timestamp with the message when user posted'() {
         given:
-        clock.setMillis(123456)
+        clock.setMillis(1000)
         timeline.post('Alice', 'I love the weather today.')
 
         when:
@@ -96,6 +96,27 @@ class TimelineTest extends Specification {
 
         then:
         aliceTimeline.get(0).text == 'I love the weather today.'
-        aliceTimeline.get(0).timestamp == 123456
+        aliceTimeline.get(0).timestamp == 1000
+    }
+
+    def 'should store timestamps with the messages when two users posted consecutively'() {
+        given:
+        clock.setMillis(1000)
+        timeline.post('Alice', 'I love the weather today.')
+        timeline.post('Bob', 'Damn! We lost!')
+        clock.setMillis(2000)
+        timeline.post('Bob', 'Good game though.')
+
+        when:
+        List<Message> aliceTimeline = timeline.view('Alice')
+        List<Message> bobTimeline = timeline.view('Bob')
+
+        then:
+        aliceTimeline.get(0).text == 'I love the weather today.'
+        aliceTimeline.get(0).timestamp == 1000
+        bobTimeline.get(0).text == 'Damn! We lost!'
+        bobTimeline.get(0).timestamp == 1000
+        bobTimeline.get(1).text == 'Good game though.'
+        bobTimeline.get(1).timestamp == 2000
     }
 }
