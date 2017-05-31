88880d7fa24cdc0bb4e03f4f7c41f1de3934adba
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-05-31 16:28:48 +0200

    Created UserRepository class.

---
+++ src/main/java/com/microtwitter/users/UserRepository.java
@@ -0,0 +1,18 @@
+package com.microtwitter.users;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRepository {
+    Map<String, User> users = new HashMap<>();
+
+    public User getOrCreate(String name) {
+        return users.containsKey(name) ? users.get(name) : newUser(name);
+    }
+
+    private User newUser(String name) {
+        User user = new User(name);
+        users.put(name, user);
+        return user;
+    }
+}
---
+++ src/test/java/com/microtwitter/users/UserRepositoryTest.groovy
@@ -0,0 +1,45 @@
+package com.microtwitter.users
+
+import spock.lang.Specification
+
+class UserRepositoryTest extends Specification {
+    private UserRepository userRepository
+
+    def setup() {
+        userRepository = new UserRepository();
+    }
+
+    def 'should return user with no name when user name is empty'() {
+        when:
+        User user = userRepository.getOrCreate('')
+
+        then:
+        user.name == ''
+    }
+
+    def 'should return user with a name when user name is not empty'() {
+        when:
+        User user = userRepository.getOrCreate('Alice')
+
+        then:
+        user.name == 'Alice'
+    }
+
+    def 'should return same user when user name is the same'() {
+        when:
+        User user1 = userRepository.getOrCreate('Alice')
+        User user2 = userRepository.getOrCreate('Alice')
+
+        then:
+        user1 == user2
+    }
+
+    def 'should return two different users when user names are different'() {
+        when:
+        User user1 = userRepository.getOrCreate('Alice')
+        User user2 = userRepository.getOrCreate('Bob')
+
+        then:
+        user1 != user2
+    }
+}
