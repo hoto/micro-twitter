f2f8687dc61fa53cacaa4fb58aeddcae69afa28a
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-06-01 02:18:47 +0200

    Using Scanner instead of Iterator for stdin. But can't mock Scanner as it is final class and it breaks test.

--- src/main/java/com/microtwitter/MicroTwitter.java
+++ src/main/java/com/microtwitter/MicroTwitter.java
@@ -1,12 +1,26 @@
 package com.microtwitter;
 
+import com.microtwitter.commands.Command;
+import com.microtwitter.commands.CommandFactory;
+import com.microtwitter.io.Parser;
+import com.microtwitter.presenters.ConsoleMessagePresenter;
+import com.microtwitter.presenters.MessagePresenter;
+import com.microtwitter.time.Clock;
+import com.microtwitter.time.SystemClock;
+import com.microtwitter.users.UserRepository;
+
 import java.io.PrintStream;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class MicroTwitter {
-    private static Iterator<String> stdin = new Scanner(System.in);
+    private static Scanner stdin = new Scanner(System.in);
     private static PrintStream stdout = System.out;
+
+    private final Clock clock = new SystemClock();
+    private final UserRepository userRepository = new UserRepository(clock);
+    private final MessagePresenter messagePresenter = new ConsoleMessagePresenter(clock);
+    private final CommandFactory commandFactory = new CommandFactory(userRepository, messagePresenter);
+    private final Parser parser = new Parser(commandFactory);
 
     public static void main(String... args) {
         new MicroTwitter().start();
@@ -18,14 +32,20 @@ public class MicroTwitter {
     }
 
     private void printHelp() {
-        stdout.println("Enter command (or exit to close):");
+        stdout.println("Enter command (exit to close):");
     }
 
     private void loop() {
         while (true) {
-            String next = stdin.next();
-            if (next.equals("exit")) break;
+            String line = stdin.nextLine();
+            if (line.equals("exit")) break;
+            executeCommand(line);
         }
+    }
+
+    private void executeCommand(String line) {
+        Command command = parser.parse(line);
+        command.execute(stdout);
     }
 
 }
