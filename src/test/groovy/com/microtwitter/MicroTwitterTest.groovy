9e8bcdee87c0706a689355f63778ad8c18a0f487
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-06-01 05:02:45 +0200

    Full e2e tests which tests 3 users (as in the specification).

--- src/test/groovy/com/microtwitter/MicroTwitterTest.groovy
+++ src/test/groovy/com/microtwitter/MicroTwitterTest.groovy
@@ -4,7 +4,7 @@ import com.microtwitter.io.Console
 import com.microtwitter.time.FixedClock
 import spock.lang.Specification
 
-import static java.util.concurrent.TimeUnit.SECONDS
+import static java.util.concurrent.TimeUnit.MINUTES
 
 class MicroTwitterTest extends Specification {
     private FixedClock clock
@@ -26,15 +26,51 @@ class MicroTwitterTest extends Specification {
         1 * console.writeOutput('Enter command (exit to close):')
     }
 
+    def 'should display a timeline'() {
+        when:
+        twitter.start()
+
+        then:
+        1 * console.writeOutput('Enter command (exit to close):')
+        1 * console.getInput() >> { time(0); 'Alice -> I love the weather today' }
+        1 * console.getInput() >> { time(5); 'Alice' }
+        1 * console.writeOutput('Alice - I love the weather today (5 minutes ago)')
+        1 * console.getInput() >> 'exit'
+    }
+
     def 'should display a wall'() {
         when:
         twitter.start()
 
         then:
-        1 * console.getInput() >> 'Alice -> Hi'
-        1 * console.getInput() >> { clock.setMillis(SECONDS.toMillis(3)); 'Alice' }
-        1 * console.getInput() >> 'exit'
         1 * console.writeOutput('Enter command (exit to close):')
-        1 * console.writeOutput('Alice - Hi (3 seconds ago)')
+        1 * console.getInput() >> { time(0); 'Alice -> I love the weather today' }
+        1 * console.getInput() >> { time(3); 'Bob -> Damn! We lost!' }
+        1 * console.getInput() >> { time(4); 'Bob -> Good game though.' }
+
+        1 * console.getInput() >> { time(5); 'Alice' }
+        1 * console.writeOutput('Alice - I love the weather today (5 minutes ago)')
+
+        1 * console.getInput() >> { time(5); 'Bob' }
+        1 * console.writeOutput('Bob - Good game though. (1 minute ago)')
+        1 * console.writeOutput('Bob - Damn! We lost! (2 minutes ago)')
+
+        1 * console.getInput() >> { time(6); "Charlie -> I'm in New York today! Anyone want to have a coffee?" }
+
+        1 * console.getInput() >> { time(7); "Charlie follows Alice" }
+        1 * console.getInput() >> { time(8); "Charlie wall" }
+        1 * console.writeOutput('Alice - I love the weather today (8 minutes ago)')
+
+        1 * console.getInput() >> { time(9); "Charlie follows Bob" }
+        1 * console.getInput() >> { time(10); "Charlie wall" }
+        1 * console.writeOutput("Charlie - I'm in New York today! Anyone want to have a coffee? (4 minutes ago)")
+        1 * console.writeOutput('Bob - Good game though. (6 minutes ago)')
+        1 * console.writeOutput('Bob - Damn! We lost! (7 minutes ago)')
+        1 * console.writeOutput('Alice - I love the weather today (10 minutes ago)')
+        1 * console.getInput() >> 'exit'
+    }
+
+    private time(long min) {
+        clock.setMillis(MINUTES.toMillis(min))
     }
 }
